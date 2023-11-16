6e69705eb4566fbdc4b627ebea7da12faae72cd3
Bruno Pereira <bruxeetos@gmail.com>
2023-11-16 13:30:54 -0300

    Estudos Yabba Script

---
+++ javascript/5-AtribuicaoDeVariaveis.js
@@ -0,0 +1,18 @@
+console.log("Atribuição de Variáveis");
+
+let nome = "Bruno";
+const sobrenome = "Pereira";
+
+//console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}` )
+
+const nomeCompleto = nome + " " + sobrenome;
+
+console.log(`Meu nome é ${nomeCompleto}`);
+
+let idade ; //declarando variável
+idade = 21; //atribuindo valor
+idade = idade + 1;
+
+console.log(idade);
---
+++ javascript/6-Listas.js
@@ -0,0 +1,19 @@
+console.log("Trabalhando com Listas");
+
+// const salvador = 'Salvador';
+// const saoPaulo = 'São Paulo';
+// const rioDeJaneiro = 'Rio de Janeiro';
+
+const listaDeDestinos = new Array(
+    'Salvador',
+    'São Paulo',
+    'Rio de Janeiro',
+    'Curitiba',
+    'Itajaí'
+);
+
+listaDeDestinos.push('Cuiabá') //Adicionando um item na lista
+
+console.log("Destinos possíveis:");
+console.log(listaDeDestinos);
+//console.log(salvador, saoPaulo, rioDeJaneiro);
