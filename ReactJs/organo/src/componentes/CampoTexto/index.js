4eb40530aa5885ee77ea5c1f21bb62b9b22c92be
Bruno Pereira <bruxeetos@gmail.com>
2023-11-20 17:53:31 -0300

    Ajuste de estados

--- ReactJs/organo/src/componentes/CampoTexto/index.js
+++ ReactJs/organo/src/componentes/CampoTexto/index.js
@@ -1,12 +1,23 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+ 
+    // let valor = 'Bruno Pereira'
+
+    const [valor, setValor] = useState('')
+
+    const aoDigitado = (evento) => {
+        setValor(evento.target.value)
+        console.log(valor)
+    }
+
     return(
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input placeholder={props.placeholder}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
