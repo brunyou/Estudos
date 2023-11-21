2e37cb6f8f55b5f2419730c2271534991c6f7d34
Bruno Pereira <bruxeetos@gmail.com>
2023-11-21 17:37:34 -0300

    3

--- SiteReact/netjuice/src/App.js
+++ SiteReact/netjuice/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Home from './componentes/Home';
 import NavBar from './componentes/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Home/>
     </div>
   );
 }
---
+++ SiteReact/netjuice/src/componentes/Home/index.js
@@ -0,0 +1,13 @@
+import './Home.css'
+
+const Home = () =>{
+    return (
+        <div className ='home'>
+            <h1>Bem vindo!</h1>
+            <h2>O que você precisa <span>hoje?</span></h2>
+
+        </div>
+    )
+}
+
+export default Home
