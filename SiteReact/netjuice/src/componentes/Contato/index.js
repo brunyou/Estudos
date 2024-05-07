7b2f6efe2967e1a67dea3aaf1d44b74533ea9363
Bruno Pereira <67168448+brunyou@users.noreply.github.com>
2024-05-07 15:16:17 -0300

    ajustes

--- SiteReact/netjuice/src/componentes/Contato/index.js
+++ SiteReact/netjuice/src/componentes/Contato/index.js
@@ -6,6 +6,7 @@ const Contato = () => {
     return(
         <section id='contato' className='contato'>
             <h1 id='titulo'>Contato</h1>
+            <div className='container'>
                 <section className='info'>
                     <h1> Eaí, vamos tirar do papel?</h1>
                     <p>Vamos marcar uma conversa:</p>
@@ -15,6 +16,7 @@ const Contato = () => {
                     </ul>
                     
                 </section>
+            </div>
             </section>
         )
 }
