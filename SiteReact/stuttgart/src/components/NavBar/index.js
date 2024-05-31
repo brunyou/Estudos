c29e51dd95196c4be368e85182ca5a3a72d631b9
Bruno Pereira <67168448+brunyou@users.noreply.github.com>
2024-05-31 13:09:07 -0300

    nav bar add

--- SiteReact/stuttgart/src/components/NavBar/index.js
+++ SiteReact/stuttgart/src/components/NavBar/index.js
@@ -1,18 +1,29 @@
 import Logo from "../../img/logo.png"
 import '../NavBar/Navbar.css'
-import NavButton from "../NavButton"
+import React,{useState} from "react"
+import {Link,NavLink} from "react-router-dom"
 
 const NavBar = () => {
+    const[menuOpen, setMenuOpen] = useState(false)
+
     return(
-        <div id='navbar'>
-            <img src={Logo} alt='Logo Stuttgart' className='logo'></img>
-            <section>
-                <NavButton href='#' nome='SOBRE NÓS'/>
-                <NavButton href='#' nome='SERVIÇOS'/>
-                <NavButton href='#' nome='CONTATOS'/>
-            </section>
+        <nav id='navbar'>
+            <Link id='logo'><img src={Logo} alt='Logo Stuttgart' className='logo'></img></Link>
+            <div className='menu' onClick={() => {
+                setMenuOpen(!menuOpen);
+            }}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul className={menuOpen ? "open" : ""}>
+                <li><NavLink to="/sobre">SOBRE NÓS</NavLink></li>
+                <li><NavLink to="/servicos">SERVIÇOS</NavLink></li>
+                <li><NavLink to="/contatos">CONTATOS</NavLink></li>
+                <li><NavLink to="/cursos">CURSOS</NavLink></li>
+            </ul>
             
-        </div>
+        </nav>
     )
 }
 
