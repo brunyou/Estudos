553273ae9bf5f91c0fa2404f725870983c894af0
Bruno Pereira <67168448+brunyou@users.noreply.github.com>
2024-05-31 18:30:54 -0300

    a

--- SiteReact/stuttgart/src/components/Home/index.js
+++ SiteReact/stuttgart/src/components/Home/index.js
@@ -1,9 +1,11 @@
+import Entrada from "../../img/entrada.jpeg"
 import "./Home.css"
 
 const Home = () => {
 return (
-    <section id='home'>
+    <section id='home' style={{ backgroundImage: `url(${Entrada})` }}>
         <h1>Oficina Especializada</h1>
+        <p>Stuttgart motors oferece atendimento a carros</p>
     </section>
 )
 }
