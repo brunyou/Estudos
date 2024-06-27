116d7809902f7bbf38d39b21fe644f19f6b34888
Bruno Pereira <67168448+brunyou@users.noreply.github.com>
2024-06-27 12:21:54 -0300

    a

--- SiteReact/stuttgart/src/App.js
+++ SiteReact/stuttgart/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
     <NavBar id='navbar'/>
     <Home/>
-    <Carousel data={slides} />
+    <Carousel />
   
   
     </div>
--- SiteReact/stuttgart/src/components/Carousel/index.jsx
+++ SiteReact/stuttgart/src/components/Carousel/index.jsx
@@ -1,31 +1,34 @@
 import React, {useState} from "react";
 import "./Carousel.css";
 import {BsArrowLeftCircleFill, BsArrowRightCircleFill} from "react-icons/bs"
+import entrada from '../../img/entrada.jpeg'
+import oficina from '../../img/oficina.jpg'
 
+const Carousel = () => {
 
-const Carousel = ({ data }) => {
+    const images = [entrada, oficina]
     const [slide, setSlide] = useState(0);
 
     const nextSlide = () => {
-        setSlide(slide === data.slides.length - 1 ? 0 : slide + 1);
+        setSlide(slide === images.length - 1 ? 0 : slide + 1);
     }
 
     const prevSlide = () => {
-         setSlide(slide === 0 ? data.slides.length - 1 : slide - 1);
+         setSlide(slide === 0 ? images.length - 1 : slide - 1);
     }
 
 
     return (
         <div className="carousel">
             <BsArrowLeftCircleFill className="arrow arrow-left" onClick={prevSlide}/>
-            {data.slides.map((item, idx) => {
+            {images.map((item, idx) => {
                 return (
-                <img src={item.src} alt={item.alt} key={idx} className={slide === idx ? "slide" : "slide slide-hidden"}></img>
+                <img src={images[idx]} alt={item.alt} key={idx} className={slide === idx ? "slide" : "slide slide-hidden"}></img>
                 );
             })}
             <BsArrowRightCircleFill className="arrow arrow-right" onClick={nextSlide}/>
             <span className="indicators">
-                {data.slides.map((_, idx) => {
+                {images.map((_, idx) => {
                 return <button key={idx} onClick={null} className={slide === idx ? "indicator" : "indicator indicator-inactive"}></button>
                 })}
             </span>
